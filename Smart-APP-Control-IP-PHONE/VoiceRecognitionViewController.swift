bf4148da556985be2b36587388d0447881818592
Chen Xiaodong <chxd99@pku.edu.cn>
2017-03-02 00:51:44 +0800

    Added an empty voice recognition view

---
+++ Smart-APP-Control-IP-PHONE/VoiceRecognitionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  VoiceRecognitionViewController.swift
+//  Smart-APP-Control-IP-PHONE
+//
+//  Created by javchen on 1/3/2017.
+//  Copyright © 2017 Cisco. All rights reserved.
+//
+
+import UIKit
+
+class VoiceRecognitionViewController: UIViewController {
+    
+}
