bc141925dc3ffce2a9f57bd40b225375e26eb164
Chen Xiaodong <chxd99@pku.edu.cn>
2017-03-02 11:31:30 +0800

    Prepared to use OpenEars

---
+++ OpenEarsHeader.h
@@ -0,0 +1,20 @@
+//
+//  OpenEarsHeader.h
+//  Smart-APP-Control-IP-PHONE
+//
+//  Created by javchen on 2/3/2017.
+//  Copyright © 2017 Cisco. All rights reserved.
+//
+
+#ifndef OpenEarsHeader_h
+#define OpenEarsHeader_h
+
+#import <OpenEars/OEPocketsphinxController.h>
+#import <OpenEars/OELanguageModelGenerator.h>
+#import <OpenEars/OEFliteController.h>
+#import <Slt/Slt.h> // Only needed if using Flite speech
+#import <OpenEars/OEEventsObserver.h>
+#import <OpenEars/OELogging.h>
+#import <OpenEars/OEAcousticModel.h>
+
+#endif /* OpenEarsHeader_h */
