0f3c658bb39c09bb9d2859edb713054b68833fe4
Chen Xiaodong <chxd99@pku.edu.cn>
2017-03-02 12:22:34 +0800

    Used OELanguageModelGenerator and OEPocketsphinxController to perform speech recognition

--- Smart-APP-Control-IP-PHONE/VoiceRecognitionViewController.swift
+++ Smart-APP-Control-IP-PHONE/VoiceRecognitionViewController.swift
@@ -10,4 +10,31 @@ import UIKit
 
 class VoiceRecognitionViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("VoiceRecognitionViewController loaded its view.")
+        
+        let lmGenerator = OELanguageModelGenerator()
+        let words = ["Hello", "World"]
+        let name = "MyLanguageModelFiles"
+        let err: Error! = lmGenerator.generateLanguageModel(from: words, withFilesNamed: name, forAcousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"))
+        
+        if (err != nil) {
+            print("Error while creating initial language model: \(err)")
+        } else {
+            let lmPath = lmGenerator.pathToSuccessfullyGeneratedLanguageModel(withRequestedName: name)
+            let dicPath = lmGenerator.pathToSuccessfullyGeneratedDictionary(withRequestedName: name)
+            
+            OELogging.startOpenEarsLogging()
+            
+            do {
+                try OEPocketsphinxController.sharedInstance().setActive(true)
+            } catch  {
+                print("Error: it wasn't possible to set the shared instance to active: \"\(error)\"")
+            }
+            
+            OEPocketsphinxController.sharedInstance().startListeningWithLanguageModel(atPath: lmPath, dictionaryAtPath: dicPath, acousticModelAtPath: OEAcousticModel.path(toModel: "AcousticModelEnglish"), languageModelIsJSGF: false)
+        }
+    }
 }
